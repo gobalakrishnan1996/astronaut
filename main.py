7fa1ac0878bebb4446523557605571c7ed4057e0
gobalakrishnan1996 <gobalakrishnangv@gmail.com>
2021-05-22 19:13:58 +0530

    Astronaut

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    height = float(input("Enter your height in meters:"))
+    if height == 1.6:
+        print ("Correct height to be an astronaut")
+    if height == 1.9:
+        print ("Correct height to be an astronaut")
+    if height < 1.6:
+        print ("Below minimum astronaut height")
+    if height > 1.9:
+        print ("Above maximum astronaut height")
+
+
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
